73262868285a4f047e4bab5cd38bb2a1805c5465
AmosAlfasi <amos13389@gmail.com>
2022-03-11 19:00:17 +0200

    add func to again button

--- script.js
+++ script.js
@@ -9,27 +9,36 @@ document.querySelector('.score').textContent = 10;
 document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value);
 */
-const secretNum = Math.trunc(Math.random() * 20) + 1;
-document.querySelector('.number').textContent = secretNum;
+let secretNum = Math.trunc(Math.random() * 5) + 1;
+
 let scoreNum = 20;
+
+document.querySelector('.again').addEventListener('click', function () {
+  document.querySelector('body').style.backgroundColor = '#222';
+  secretNum = Math.trunc(Math.random() * 5) + 1;
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.message').textContent = 'Start guessing...';
+});
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   // console.log(guess, typeof guess);
   //NO input
+
   if (!guess) {
     document.querySelector('.message').textContent = 'No Number';
   }
   //correct number
   else if (guess === secretNum) {
     if (scoreNum > 0) {
+      document.querySelector('.number').textContent = secretNum;
       document.querySelector('.message').textContent = 'You are corect !!!';
 
       document.querySelector('body').style.backgroundColor = '#D49E0E';
 
       document.querySelector('.number').style.width = '30rem';
-      document.querySelector('.number').style.padding = '5rem';
-      document.querySelector('.number').style.fontSize = '10rem';
+      //document.querySelector('.number').style.padding = '5rem';
+      // document.querySelector('.number').style.fontSize = '10rem';
     }
   }
   //guess is too high
